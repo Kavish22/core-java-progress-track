4606e7d5201a4e29f3780049c92a607207f84e34
Kavish22 <111997006+Kavish22@users.noreply.github.com>
2023-03-18 11:11:35 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    
+// so this is the first program of my java, in which i will print an Output "Hello World".
+
+    public static void main(String[] args) {
+   
+        System.out.println("Hello World");
+     }
+  }
